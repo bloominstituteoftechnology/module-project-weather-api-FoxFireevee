55396cb8adbd16c6347b87e6bb526dba1584c7f9
Hailey Murphy <foxfireevee@gmail.com>
2024-02-11 05:01:50 -0500

    Halfway there. Built the main panel, the loading features, and need to finish the daily panels

--- frontend/index.js
+++ frontend/index.js
@@ -16,6 +16,69 @@ async function moduleProject4() {
 
   // 👉 Tasks 1 - 5 go here
 
+  document.querySelector('#weatherWidget').style.display = 'none';
+
+  document.querySelector('#citySelect').addEventListener('change', async evt => {
+    console.log('selection changed');
+    try {
+      document.querySelector('#citySelect').setAttribute('disabled', 'disabled');
+      document.querySelector('#weatherWidget').style.display = 'none';
+      document.querySelector('.info').textContent = 'Fetching weather data...';
+      
+      console.log(evt.target.value);
+      let city = evt.target.value;
+      let url = `http://localhost:3003/api/weather?city=${city}`;
+      
+      const res = await axios.get(url);
+
+      document.querySelector('#weatherWidget').style.display = 'block';
+      document.querySelector('.info').textContent = '';
+      evt.target.removeAttribute('disabled');
+
+      console.log(res)
+
+      let { data } = res
+
+      //changes the 'Feels like' temp from the hardcoded default to the api's data of the apparent temp.
+      document.querySelector('#apparentTemp div:nth-child(2)').textContent = `${data.current.apparent_temperature}°`;
+
+      //changes the photo icon of the biggest panel
+      document.querySelector('#todayDescription').textContent = descriptions.find(d => d[0] === data.current.weather_description)[1];
+
+      //changes the min and max of the biggest panel
+      document.querySelector('#todayStats div:nth-child(1)').textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`;
+
+      //changes the precipitation of the biggest panel
+      document.querySelector('#todayStats div:nth-child(2)').textContent = `Precipitation: ${data.current.precipitation_probability * 100}%`;
+
+      //changes the humidity of the biggest panel
+      document.querySelector('#todayStats div:nth-child(3)').textContent = `Humidity: ${data.current.humidity}%`;
+
+      //changes the wind of the biggest panel
+      document.querySelector('#todayStats div:nth-child(4)').textContent = `Wind: ${data.current.wind_speed}m/s`;
+
+      //changes name of city at bottom of biggest panel
+      document.querySelector('#location').firstElementChild.textContent = city;
+
+      data.forecast.daily.forEach((day, idx) => {
+        let card = document.querySelectorAll('.next-day')[idx];
+
+        let weekDay = card.children[0];
+        let apparent = card.children[1];
+        let temp = card.children[2];
+        let precipitation = card.children[3];
+
+        weekDay.textContent = getWeekDay(day.date)
+        apparent.textContent = descriptions.find(a => a[0] === day.weather_description)[1];
+        temp.textContent = `${day.temperature_min}°/${day.temperature_max}°`;
+        precipitation.textContent = `Precipitation: ${day.precipitation_probability * 100}%`;
+        
+      })
+    } catch (err) {
+      console.log(err.message);
+    };
+  })
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 }
